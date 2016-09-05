acef359f33bc88e2b89c0fc48dca1a4082ec2218
Eric Stevens <Eric Stevens>
2016-09-05 08:07:48 -0400

    make a webservice resolvie issue #5

--- websend.py
+++ websend.py
@@ -1,11 +1,16 @@
 #!flask/bin/python
 from flask import Flask
+import pyUserMessages as pums
 
 app = Flask(__name__)
 
 @app.route('/send/<string:message>/<string:toEmail>', methods=['GET'])
 def index(message, toEmail):
-    return "Send message:" + message + " to " + toEmail
 
+    msgs = pums.pyUserMessages()
+    msg = msgs.get(message)
+    msg.send(toEmail)
+    return 'OK'
+    
 if __name__ == '__main__':
     app.run(debug=True)
